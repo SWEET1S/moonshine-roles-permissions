5e273b0d2dbc91cdf5adf16f65f422841f495ec6
SWEETIS <abalan1510it@gmail.com>
2023-07-21 20:53:25 +0300

    fix(components): Hidden if not exist Policy

--- resources/views/form-components/permissions.blade.php
+++ resources/views/form-components/permissions.blade.php
@@ -1,4 +1,9 @@
 @if($item->exists)
+
+    @php
+        $permissions = Spatie\Permission\Models\Permission::all()->pluck('name')->toArray();
+    @endphp
+
     <div>
         <div class="text-lg my-4">{{ $element->label() }}</div>
 
@@ -9,14 +14,19 @@
 
             @foreach(moonshine()->getResources() as $resource)
 
+                @php
+                    $resourceName = explode('\\',get_class($resource))[count(explode('\\',get_class($resource))) - 1];
+                @endphp
+
                 <div>
-                    <div class="text-md my-4">{{ $resource->title() }}</div>
+                    <div class="text-md my-4 {{ strpos(implode($permissions), $resourceName) === false ? 'hidden' : '' }}">
+                        {{ $resource->title() }}
+                    </div>
 
                     <div class="flex items-center justify-start space-x-4">
                         @foreach($resource->gateAbilities() as $ability)
 
                             @php
-                                $resourceName = explode('\\',get_class($resource))[count(explode('\\',get_class($resource))) - 1];
                                 $permission = $resourceName.".".$ability
                             @endphp
 
@@ -24,7 +34,7 @@
                                 name="permissions[{{ $permission }}]"
                                 :label="$ability"
                                 :beforeLabel="true"
-                                class="form-group-inline"
+                                class="form-group-inline {{ in_array($permission, $permissions) ?: 'hidden'}}"
                                 :id="str('permissions_' . get_class($resource) . '_' . $ability)->slug('_')"
                             >
 
@@ -33,7 +43,7 @@
                                     type="checkbox"
                                     name="permissions[{{ $permission }}]]"
                                     value="1"
-                                    :checked="$item->hasPermissionTo($permission)"
+                                    :checked="in_array($permission, $permissions) ? $item->hasPermissionTo($permission) : false"
                                 />
                             </x-moonshine::form.input-wrapper>
                         @endforeach
