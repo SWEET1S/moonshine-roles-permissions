576ec866b497192cc90914d93f852fcd8fb3913d
SWEETIS <abalan1510it@gmail.com>
2023-07-24 00:54:13 +0300

    fix(routes): Protection with middleware

--- routes/moonshine-roles-permissions.php
+++ routes/moonshine-roles-permissions.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Sweet1s\MoonshineRolesPermissions\Http\Controllers\MoonShineRolesPermissionsController;
 
+$middlewares = collect(config('moonshine.route.middleware'))
+    ->reject(static fn($middleware): bool => $middleware === 'web')
+    ->toArray();
 
-Route::post('moonshine-roles-permissions/roles/{role}/permissions', [MoonShineRolesPermissionsController::class, 'attachPermissionsToRole'])
-    ->name('moonshine-roles-permissions.roles.attach-permissions-to-role');
+Route::as('moonshine-roles-permissions.roles.')->middleware($middlewares)->group(function () {
+    Route::post('moonshine-roles-permissions/roles/{role}/permissions', [MoonShineRolesPermissionsController::class, 'attachPermissionsToRole'])
+        ->name('roles.attach-permissions-to-role');
+});
